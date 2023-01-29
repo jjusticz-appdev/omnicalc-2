34bd9dcb947e9800eb7d422df978c2e038c5189a
jjusticz <jjusticz@uchicago.edu>
2023-01-29 21:54:18 +0000

    Final commit

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -1,8 +1,16 @@
 class MathController < ApplicationController
 
-  def add
+  def add_results
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @first_num + @second_num
     render({ :template => "layouts/add_results.html.erb"})
   end
+
+  def blank_add_form
+    render({ :template => "layouts/add_form.html.erb"})
+  end
+
 
   def blank_subtract_form
     render({ :template => "layouts/subtract_form.html.erb"})
@@ -15,11 +23,25 @@ class MathController < ApplicationController
     render({ :template => "layouts/subtract_results.html.erb"})
   end
 
-  def multiply
+  def blank_multiply_form
+    render({ :template => "layouts/multiply_form.html.erb"})
+  end
+
+  def multiply_results
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @second_num * @first_num
     render({ :template => "layouts/multiply_results.html.erb"})
   end
 
-  def divide
+  def blank_divide_form
+    render({ :template => "layouts/divide_form.html.erb"})
+  end
+
+  def divide_results
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+    @result = @first_num / @second_num
     render({ :template => "layouts/divide_results.html.erb"})
   end
 
